8a06ee35f3586105fa6835773f89d24d0248f45b
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-11 01:41:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,15 +27,23 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  case 13:
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-  case 18:
-  case 19:
+  switch(age){
+    case 13:
     return "You are a teenager"
-    break;
-  default:
+    case 14:
+    return "You are a teenager"
+    case 15:
+    return "You are a teenager"
+    case 16:
+    return "You are a teenager"
+    case 17:
+    return "You are a teenager"
+    case 18:
+    return "You are a teenager"
+    case 19:
+    return "You are a teenager"
+        break;
+    default:
     return "You have an age"
+  }
 }
